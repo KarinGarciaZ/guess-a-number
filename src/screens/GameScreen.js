5b50dc5615b9dfadc90feec14d1076bec402f2f8
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-09-22 22:35:51 -0500

    Added screen to play anfter chose a number.

---
+++ src/screens/GameScreen.js
@@ -0,0 +1,64 @@
+import React, {useState, useRef} from 'react'
+import { StyleSheet, View, Text, Button, Alert }  from 'react-native'
+import ChosenNumber from '../components/ChosenNumber'
+import Card from '../components/Card'
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min
+  if( randomNumber === exclude )
+    return generateRandomBetween(min, max, exclude)
+  return randomNumber
+}
+
+const GameScreen = props => { 
+  let [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice))
+  const currentLow = useRef(1)
+  const currentHigh = useRef(100)
+
+  const nextGuessHandler = direction => {
+    if((direction === 'lower' && currentGuess < props.userChoice) || (direction === 'greater' && currentGuess > props.userChoice)) {
+      Alert.alert('Dont lie!', 'You know this is wrong...', [{text: 'Close', style: 'cancel'}])
+      return
+    }
+
+    if(direction === 'lower')
+      currentHigh.current = currentGuess
+    else 
+      currentLow.current = currentGuess
+    
+    const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
+    setCurrentGuess(nextNumber)
+  }
+
+  return(
+    <View style={styles.screen}>
+      <Text style={styles.title}>Oponent's Guess</Text>
+      <ChosenNumber number={currentGuess} />
+      <Card styles={styles.card}>
+        <Button title='Lower' onPress={() => nextGuessHandler('lower')} />
+        <Button title='Greater' onPress={() => nextGuessHandler('greater')} />
+      </Card>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10
+  }, 
+  title: {
+    fontSize: 20
+  },
+  card: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '70%',
+    elevation: 10
+  }
+})
+
+export default GameScreen
